d26792b8c07ba8a1c092958ee52f2b5d6277de80
Joaquín Ignacio Ossandón Gómez <joaquinossandong@gmail.com>
2024-06-06 02:10:40 -0400

    fix: again trying with envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+type ENV = {
+  // replace `MY_KV` with your KV namespace
+  TURSO_DATABASE_URL: string;
+  TURSO_AUTH_TOKEN: string;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
