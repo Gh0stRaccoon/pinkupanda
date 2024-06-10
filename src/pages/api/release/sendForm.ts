a1f18f724249914b4586f9874006d3742199b0dc
Joaquín Ignacio Ossandón Gómez <joaquinossandong@gmail.com>
2024-06-10 14:11:41 -0400

    try catch added

--- src/pages/api/release/sendForm.ts
+++ src/pages/api/release/sendForm.ts
@@ -7,20 +7,29 @@ export const POST: APIRoute = async ({ locals, request }) => {
   const data = await request.json();
   const email = data.email;
 
-  const tursoClient = createClient({
-    url: db_url,
-    authToken: token,
-  });
+  try {
+    const tursoClient = createClient({
+      url: db_url,
+      authToken: token,
+    });
 
-  tursoClient.execute({
-    sql: "INSERT INTO collected_emails(ID, email) VALUES (null, ?)",
-    args: [email],
-  });
+    tursoClient.execute({
+      sql: "INSERT INTO collected_emails(ID, email) VALUES (null, ?)",
+      args: [email],
+    });
 
-  return new Response(
-    JSON.stringify({
-      preregister: "ok",
-      email,
-    })
-  );
+    return new Response(
+      JSON.stringify({
+        preregister: "ok",
+        email,
+      })
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        preregister: "Error",
+        error,
+      })
+    );
+  }
 };
